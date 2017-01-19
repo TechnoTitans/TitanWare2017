70b8f412ca47c3bf6dddb9a1b52853a4cb18a94c
panda <panda@DESKTOP-3STCIA5>
2017-01-19 17:46:09 -0500

    Added shooter class and spruce code

--- src/org/usfirst/frc/team1683/sensors/LimitSwitch.java
+++ src/org/usfirst/frc/team1683/sensors/LimitSwitch.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team1683.sensors;
 
 public class LimitSwitch extends edu.wpi.first.wpilibj.DigitalInput {
 
-  public LimitSwitch(int portNumber) { super(portNumber); }
+	public LimitSwitch(int portNumber) {
+		super(portNumber);
+	}
 
-  public boolean isPressed() { return super.get(); }
+	public boolean isPressed() {
+		return super.get();
+	}
 }
---
+++ src/org/usfirst/frc/team1683/shooter/Shooter.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team1683.shooter;
+
+import edu.wpi.first.wpilibj.TalonSRX;
+
+public class Shooter {
+	
+	TalonSRX shooterWheel;
+	public Shooter(int channel) {
+		this.shooterWheel = new TalonSRX(channel);
+	}
+	
+	//public double getSpeed() {
+		// TODO until encoders are done
+	//}
+	//TODO PID LOOP
+	public void setSpeed(double rotationSpeed){
+		shooterWheel.set(rotationSpeed);
+	}
+}
