7946cc77d18cc3877255bd6f0dc10a9c0ba7f387
prakol16 <praneeth.kolichala@gmail.com>
2017-01-21 16:29:38 -0500

    Undid Yi's change of setting all the axes (x, y, and z) to zero
    For some reason, in the commit "Fixed Winch" Yi set all the axis
    constants to zero, instead of the 1, 2, and 3 that they should be.

--- src/org/usfirst/frc/team1683/driverStation/DriverStation.java
+++ src/org/usfirst/frc/team1683/driverStation/DriverStation.java
@@ -7,8 +7,9 @@ import edu.wpi.first.wpilibj.Joystick;
 public class DriverStation {
 
 	public static final int XAxis = 0;
-	public static final int YAxis = 0;
-	public static final int ZAxis = 0;
+	public static final int YAxis = 1;
+	public static final int ZAxis = 2;
+	public static final int dialAxis = 3;
 	public static boolean[][] lasts = new boolean[3][11];
 	public static Joystick leftStick = new Joystick(HWR.LEFT_JOYSTICK);
 	public static Joystick rightStick = new Joystick(HWR.RIGHT_JOYSTICK);
