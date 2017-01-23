8227e50a0e2beb592f42bb407809ef42318103fe
panda <panda@DESKTOP-3STCIA5>
2017-01-23 17:35:38 -0500

    Spruce code

--- src/org/usfirst/frc/team1683/sensors/Sensor.java
+++ src/org/usfirst/frc/team1683/sensors/Sensor.java
@@ -5,6 +5,6 @@ package org.usfirst.frc.team1683.sensors;
  */
 public interface Sensor {
 
-  // TODO: the rest of this class
-  public double getRaw();
+	// TODO: the rest of this class
+	public double getRaw();
 }
