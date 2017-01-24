d660ee26d8a28b7c5ecbcd45d0fbf493dd7c0552
panda <panda@DESKTOP-3STCIA5>
2017-01-24 10:23:09 -0500

    Spruce Code

--- src/org/usfirst/frc/team1683/sensors/QuadEncoder.java
+++ src/org/usfirst/frc/team1683/sensors/QuadEncoder.java
@@ -7,41 +7,45 @@ import com.ctre.CANTalon.FeedbackDevice;
 
 public class QuadEncoder implements Encoder {
 
-  private TalonSRX talonSRX;
-  private double wheelRadius;
+	private TalonSRX talonSRX;
+	private double wheelRadius;
 
-  public QuadEncoder(TalonSRX talonSRX, double wheelRadius) {
-    this.talonSRX = talonSRX;
-    this.talonSRX.setFeedbackDevice(
-        FeedbackDevice.CtreMagEncoder_Relative);
+	public QuadEncoder(TalonSRX talonSRX, double wheelRadius) {
+		this.talonSRX = talonSRX;
+		this.talonSRX.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 
-    this.wheelRadius = wheelRadius;
+		this.wheelRadius = wheelRadius;
 
-  }
+	}
 
-  /**
-   * The total distance that the motor has traveled
-   * Multiplies rotations by 2*pi*r where r = wheel radius
-   * @return total distance
-   */
-  @Override
-  public double getDistance() {
-    return talonSRX.getPosition() * 2 * Math.PI * wheelRadius;
-  }
-  /**
-   * Just calls talonSRX.getSpeed()
-   * @return The speed of the talon in RPM
-   */
-  @Override
-  public double getSpeed() {
-    return talonSRX.getSpeed();
-  }
+	/**
+	 * The total distance that the motor has traveled Multiplies rotations by
+	 * 2*pi*r where r = wheel radius
+	 * 
+	 * @return total distance
+	 */
+	@Override
+	public double getDistance() {
+		return talonSRX.getPosition() * 2 * Math.PI * wheelRadius;
+	}
 
-  @Override
-  public void reset() {
-    // this.talonSRX.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
-    talonSRX.setPosition(0);
-  }
+	/**
+	 * Just calls talonSRX.getSpeed()
+	 * 
+	 * @return The speed of the talon in RPM
+	 */
+	@Override
+	public double getSpeed() {
+		return talonSRX.getSpeed();
+	}
 
-  public TalonSRX getTalon() { return talonSRX; }
+	@Override
+	public void reset() {
+		// this.talonSRX.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		talonSRX.setPosition(0);
+	}
+
+	public TalonSRX getTalon() {
+		return talonSRX;
+	}
 }
