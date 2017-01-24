9af7d64306d9733c6da5872ec779c423e3f97c93
Panda7777 <pandaliu2011@gmail.com>
2017-01-24 10:25:33 -0500

    Spruce code and update winch

--- src/org/usfirst/frc/team1683/sensors/AnalogAccel.java
+++ src/org/usfirst/frc/team1683/sensors/AnalogAccel.java
@@ -1,18 +1,17 @@
 package org.usfirst.frc.team1683.sensors;
 
-public class AnalogAccel
-    extends edu.wpi.first.wpilibj.AnalogAccelerometer {
-  public static final double SENSITIVITY = 3.3 / 6;
-  public static final double ZERO_G_IN_VOLTS = 1.65;
+public class AnalogAccel extends edu.wpi.first.wpilibj.AnalogAccelerometer {
+	public static final double SENSITIVITY = 3.3 / 6;
+	public static final double ZERO_G_IN_VOLTS = 1.65;
 
-  public AnalogAccel(int channel) {
-    super(channel);
-    super.setSensitivity(SENSITIVITY);
-    super.setZero(ZERO_G_IN_VOLTS);
-  }
+	public AnalogAccel(int channel) {
+		super(channel);
+		super.setSensitivity(SENSITIVITY);
+		super.setZero(ZERO_G_IN_VOLTS);
+	}
 
-  @Override
-  public double getAcceleration() {
-    return super.getAcceleration();
-  }
+	@Override
+	public double getAcceleration() {
+		return super.getAcceleration();
+	}
 }
