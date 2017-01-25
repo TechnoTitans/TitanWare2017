3bfddc5ebc0f382bdd57772c903953f09cc3b884
Panda7777 <pandaliu2011@gmail.com>
2017-01-25 12:12:48 -0500

    Added solenoid test program and also required changes

--- src/org/usfirst/frc/team1683/test/SolenoidTest.java
+++ src/org/usfirst/frc/team1683/test/SolenoidTest.java
@@ -1,12 +1,16 @@
 package org.usfirst.frc.team1683.test;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import org.usfirst.frc.team1683.robot.HWR;
+
 public class SolenoidTest {
 	Solenoid solenoid;
-	public SolenoidTest(){
+
+	public SolenoidTest() {
 		solenoid = new Solenoid(HWR.DEFAULT_MODULE_CHANNEL, HWR.GEAR_PISTON_CHANNEL);
 	}
-	public void test(){
+
+	public void test() {
 		solenoid.set(true);
 	}
 }
