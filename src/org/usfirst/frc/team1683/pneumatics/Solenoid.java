b4596384fd64eda4961a06511c0c2bcbd65ab265
panda <panda@DESKTOP-3STCIA5>
2017-01-24 19:57:43 -0500

    Got rid of SingleSolenoid, pneumatics is done

--- src/org/usfirst/frc/team1683/pneumatics/Solenoid.java
+++ src/org/usfirst/frc/team1683/pneumatics/Solenoid.java
@@ -2,9 +2,11 @@ package org.usfirst.frc.team1683.pneumatics;
 
 public class Solenoid extends edu.wpi.first.wpilibj.Solenoid {
 
-  public Solenoid(int moduleChannel, int channel) {
-    super(moduleChannel, channel);
-  }
+	public Solenoid(int moduleChannel, int channel) {
+		super(moduleChannel, channel);
+	}
 
-  public boolean isExtended() { return super.get(); }
+	public boolean isExtended() {
+		return super.get();
+	}
 }
