379ed6c9206b25650955fbea758330dc1f5de8b7
Panda7777 <pandaliu2011@gmail.com>
2017-01-24 19:17:22 -0500

    Autonomous Update

--- src/org/usfirst/frc/team1683/robot/TechnoTitan.java
+++ src/org/usfirst/frc/team1683/robot/TechnoTitan.java
@@ -2,17 +2,15 @@
 package org.usfirst.frc.team1683.robot;
 
 import org.usfirst.frc.team1683.autonomous.AutonomousSwitcher;
+import org.usfirst.frc.team1683.autonomous.DoNothing;
 import org.usfirst.frc.team1683.driveTrain.MotorGroup;
 import org.usfirst.frc.team1683.driveTrain.TalonSRX;
 import org.usfirst.frc.team1683.driveTrain.TankDrive;
 import org.usfirst.frc.team1683.sensors.BuiltInAccel;
 import org.usfirst.frc.team1683.sensors.Gyro;
-import org.usfirst.frc.team1683.sensors.LinearActuator;
 import org.usfirst.frc.team1683.sensors.QuadEncoder;
 import org.usfirst.frc.team1683.sensors.PressureReader;
 import org.usfirst.frc.team1683.vision.LightRing;
-
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -26,10 +24,11 @@ public class TechnoTitan extends IterativeRobot {
 	Timer endGameTimer;
 	PressureReader pressureReader;
 	LightRing lightRing;
+	DoNothing auto;
 	Compressor compressor = new Compressor(1);
 	@Override
 	public void robotInit() {
-		//drive train
+		
 		Gyro gyro = new Gyro(HWR.GYRO);
 
 		TalonSRX leftETalonSRX = new TalonSRX(HWR.LEFT_DRIVE_TRAIN_FRONT, LEFT_REVERSE);
@@ -42,7 +41,7 @@ public class TechnoTitan extends IterativeRobot {
 
 		drive = new TankDrive(leftGroup, rightGroup, gyro);
 
-		// END DRIVE TRAIN
+		
 		endGameTimer = new Timer();
 
 		BuiltInAccel accel = new BuiltInAccel();
@@ -50,8 +49,9 @@ public class TechnoTitan extends IterativeRobot {
 
 		lightRing = new LightRing(HWR.LIGHT_RING);
 		lightRing.set(1);
+		
+		switcher = new AutonomousSwitcher(drive, accel);
 
-		// switcher = new AutonomousSwitcher(drive, accel, actuator);
 //		CameraServer server = CameraServer.getInstance();
 //		server.setQuality(50);
 //		server.startAutomaticCapture("cam1");
@@ -59,11 +59,11 @@ public class TechnoTitan extends IterativeRobot {
 
 	@Override
 	public void autonomousInit() {
-		drive.moveDistance(1000);
 	}
 
 	@Override
 	public void autonomousPeriodic() {
+		auto.run();
 	}
 
 	@Override
