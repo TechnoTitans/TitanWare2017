e072d1c78acf24c889dde937a8facb06e1ea6ac3
prakol16 <praneeth.kolichala@gmail.com>
2017-02-17 16:22:33 -0500

    Cleaning up

--- src/org/usfirst/frc/team1683/driveTrain/TankDrive.java
+++ src/org/usfirst/frc/team1683/driveTrain/TankDrive.java
@@ -24,9 +24,7 @@ public class TankDrive implements DriveTrain {
 		this.gyro = gyro;
 		// this.gyro.reset();
 	}
-	/**
-	 * Sets the speed.
-	 */
+	
 	@Override
 	public void set(double speed) {
 		left.set(speed);
@@ -40,28 +38,12 @@ public class TankDrive implements DriveTrain {
 	public void setRight(double speed) {
 		right.set(speed);
 	}
-
-	/**
-	 * Sets speed of each motor group
-	 * 
-	 * @param leftSpeed
-	 *            -- speed of left motor group
-	 * @param rightSpeed
-	 *            -- speed of right motor group
-	 */
+	
 	public void set(double leftSpeed, double rightSpeed) {
 		left.set(leftSpeed);
 		right.set(rightSpeed);
 	}
-
-	/**
-	 * Turns in place
-	 * 
-	 * @param right
-	 *            -- True if should turn right (clockwise), false if left
-	 * @param speed
-	 *            -- Speed
-	 */
+	
 	public void turnInPlace(boolean right, double speed) {
 		if (right) {
 			set(speed, -speed);
@@ -69,10 +51,7 @@ public class TankDrive implements DriveTrain {
 			set(-speed, speed);
 		}
 	}
-
-	/**
-	 * Stop the drive train.
-	 */
+	
 	@Override
 	public void stop() {
 		left.enableBrakeMode(true);
@@ -81,9 +60,6 @@ public class TankDrive implements DriveTrain {
 		right.stop();
 	}
 
-	/**
-	 * Stop without braking.
-	 */
 	@Override
 	public void coast() {
 		left.enableBrakeMode(false);
@@ -92,9 +68,6 @@ public class TankDrive implements DriveTrain {
 		right.stop();
 	}
 
-	/**
-	 * Start driving.
-	 */
 	@Override
 	public void driveMode(double leftSpeed, double rightSpeed) {
 		left.enableBrakeMode(false);
