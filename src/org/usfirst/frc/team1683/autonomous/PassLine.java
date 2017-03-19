4b40634961d9b254b20d91d4447c6cb636e2daaf
Panda7777 <pandaliu2011@gmail.com>
2017-03-19 13:04:39 -0400

    Passline goes forward, go back, and turn

--- src/org/usfirst/frc/team1683/autonomous/PassLine.java
+++ src/org/usfirst/frc/team1683/autonomous/PassLine.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1683.autonomous;
 
 import org.usfirst.frc.team1683.driveTrain.DriveTrainMover;
+import org.usfirst.frc.team1683.driveTrain.DriveTrainTurner;
 import org.usfirst.frc.team1683.driveTrain.TankDrive;
 
 /**
@@ -11,30 +12,43 @@ import org.usfirst.frc.team1683.driveTrain.TankDrive;
  */
 public class PassLine extends Autonomous {
 	public final double distance = 300; // inches
+	public final double backup = -250;
 	DriveTrainMover mover;
+	DriveTrainTurner turner;
+	boolean turnRight;
 
-	public PassLine(TankDrive tankDrive) {
+	public PassLine(TankDrive tankDrive, boolean turnRight) {
 		super(tankDrive);
+		this.turnRight = turnRight;
 	}
 
 	public void run() {
 		switch (presentState) {
-			case INIT_CASE:
-				mover = new DriveTrainMover(tankDrive, distance, 0.5);
-				nextState = State.DRIVE_FORWARD;
-				break;
-			case DRIVE_FORWARD:
-				mover.runIteration();
-				if (mover.areAnyFinished()) {
-					tankDrive.stop();
-					nextState = State.END_CASE;
-				}
-				break;
-			case END_CASE:
-				tankDrive.set(0);
-				break;
-			default:
-				break;
+		case INIT_CASE:
+			mover = new DriveTrainMover(tankDrive, distance, 0.5);
+			nextState = State.DRIVE_FORWARD;
+			break;
+		case DRIVE_FORWARD:
+			mover.runIteration();
+			if (mover.areAnyFinished()) {
+				tankDrive.stop();
+				mover = new DriveTrainMover(tankDrive, backup, 0.5);
+				nextState = State.BACK_UP;
+			}
+			break;
+		case BACK_UP:
+			mover.runIteration();
+			if (mover.areAnyFinished()) {
+				tankDrive.stop();
+				turner = new DriveTrainTurner(tankDrive, (turnRight ? -1 : 1) * 56, 0.2);
+				nextState = State.END_CASE;
+			}
+			break;
+		case END_CASE:
+			tankDrive.stop();
+			break;
+		default:
+			break;
 		}
 		presentState = nextState;
 	}
