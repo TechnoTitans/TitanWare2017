fdaf9a377da1b4a184d730133ef344400600e4ed
Panda7777 <pandaliu2011@gmail.com>
2017-04-26 00:08:36 -0400

    Curved Drive Test and stripped down template code of TitanWare2017

---
+++ src/org/usfirst/frc/team1683/driveTrain/FollowPath.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team1683.driveTrain;
+
+import org.usfirst.frc.team1683.constants.Constants;
+
+public class FollowPath {
+	private TankDrive drive;
+	private PathPoint[] pathPoints = { new PathPoint(0, 0, false), new PathPoint(0, 20, false),
+			new PathPoint(20, 20, false) };
+
+	private PathPoint point1, point2, point3;
+	private DriveTrainMover mover;
+	public FollowPath(TankDrive drive) {
+		this.drive = drive;
+
+		point1 = pathPoints[0];
+		point2 = pathPoints[1];
+		point3 = pathPoints[2];
+		
+		mover = new DriveTrainMover(drive, calDistTravel()[0], calDistTravel()[1], 0.25);
+	}
+	
+	public void move(){
+		mover.runIteration();
+		if(mover.areAllFinished()){
+			drive.stop();
+		}
+	}
+
+	private double[] calDistTravel() {
+		double distance1 = (calRadius() + (curveDirection() ? 1 : -1) * Constants.ROBOT_WIDTH / 2)
+				* PathPoint.getAngleTwoPoints(point1, point2, calRadius());
+		double distance2 = (calRadius() + (curveDirection() ? -1 : 1) * Constants.ROBOT_WIDTH / 2)
+				* PathPoint.getAngleTwoPoints(point1, point2, calRadius());
+		return new double[] { distance1, distance2 };
+	}
+
+	// return true if right and false if left
+	private boolean curveDirection() {
+		return 0 < point1.getX() + point2.getX() + point3.getX();
+	}
+
+	private double calRadius() {
+		double side1 = PathPoint.getDistance(point1, point2);
+		double side2 = PathPoint.getDistance(point2, point3);
+		double side3 = PathPoint.getDistance(point3, point1);
+
+		double semiPeri = (side1 + side2 + side3) / 2;
+		double area = Math.sqrt(semiPeri * (semiPeri - side1) * (semiPeri - side2) * (semiPeri - side3));
+
+		double radius = side1 * side2 * side3 / (area * 4);
+		return radius;
+	}
+}
--- src/org/usfirst/frc/team1683/sensors/Sensor.java
+++
@@ -1,5 +0,0 @@
-package org.usfirst.frc.team1683.sensors;
-
-public interface Sensor {
-	public double getRaw();
-}
